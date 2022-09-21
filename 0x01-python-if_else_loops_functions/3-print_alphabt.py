4c3c53ff4482e1c659cfe843786c214e502c3a2d
Abdulkareem Ridwan Opeyemi <55938645+abuphawzww@users.noreply.github.com>
2022-09-21 04:44:16 +0100

    project 2 done

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for x in range(97, 123):
-    if (x != 113 and x != 101):
-        print("{:c}".format(x), end="")
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
